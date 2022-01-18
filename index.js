015ece87384125c09c33b44b831a99f73d975160
mic4x <95673380+mic4x@users.noreply.github.com>
2022-01-18 03:32:59 -0500

    Puzzle#4
    Ransom Letter

---
+++ index.js
@@ -0,0 +1,14 @@
+//a loop that check each value of the string is only repeated once, if it repeated more than once than ransomNote=false and cannot be constructed. 
+
+//Basically meaning the ransomnote cannot be created using the message in the magazine.
+
+
+x=magazine.length
+
+for (i=0; i<= x; i++)
+
+if (magazine[i] = ransom[i])
+{//Code that counts amount of time that character appears
+//if that character is count is greater than 1 than break the loop and console.log('This cannot be a ransom note.)
+
+}
